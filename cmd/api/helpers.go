662ec8e0c516473cf042384fe8dfe626c55def3f
Jennessa Sierra <2021153908@ub.edu.bz>
2026-02-11 23:53:45 -0600

    Add encoding json v4

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"net/http"
 )
 
-func (a *applicationDependencies)writeJSON(w http.ResponseWriter,
-                                           status int, data any,
-                                           headers http.Header) error  {
-    jsResponse, err := json.Marshal(data)
-    if err != nil {
-        return err
-    }
-    jsResponse = append(jsResponse, '\n')
-    // additional headers to be set
-    for key, value := range headers {
-        w.Header()[key] = value
-        //w.Header().Set(key, value[0])
-    }
-    // set content type header
-    w.Header().Set("Content-Type", "application/json")
-    // explicitly set the response status code
-    w.WriteHeader(status) 
-    w.Write(jsResponse)
+func (a *applicationDependencies) writeJSON(w http.ResponseWriter,
+	status int, data any,
+	headers http.Header) error {
 
-    return nil
+	jsResponse, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	jsResponse = append(jsResponse, '\n')
+
+	// additional headers to be set
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// set content type header
+	w.Header().Set("Content-Type", "application/json")
+
+	// explicitly set the response status code
+	w.WriteHeader(status)
+	_, err = w.Write(jsResponse)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
