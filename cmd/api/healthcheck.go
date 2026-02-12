f121adb61fb587b6ca21d6abfa21b8127b464aac
Jennessa Sierra <2021153908@ub.edu.bz>
2026-02-11 23:24:03 -0600

    Add encoding json v1

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,8 @@ import (
 
 func (a *applicationDependencies) healthcheckHandler(w http.ResponseWriter,
 	r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", a.config.environment)
-	fmt.Fprintf(w, "version: %s\n", appVersion)
+	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
+	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(jsResponse))
 }
