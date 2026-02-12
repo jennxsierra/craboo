205134b799563c34ac4a9a6f054bfcebb9ae43fc
Jennessa Sierra <2021153908@ub.edu.bz>
2026-02-11 23:37:51 -0600

    Add encoding json v2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (a *applicationDependencies) healthcheckHandler(w http.ResponseWriter,
 	r *http.Request) {
-	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
-	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
+	data := map[string]string{
+		"status":      "available",
+		"environment": a.config.environment,
+		"version":     appVersion,
+	}
+
+	jsResponse, err := json.Marshal(data)
+
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(jsResponse))
+	w.Write(jsResponse)
+
 }
